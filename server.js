6e54b85cc21493a14e332def8c2ffbaa6a3efc41
Pratyushgupta2003 <pratyushgupta20032003@gmail.com>
2024-05-11 01:29:51 +0530

    ..

--- server.js
+++ server.js
@@ -20,14 +20,14 @@ connectDB();
 const __filename = fileURLToPath(import.meta.url);
  const __dirname = path.dirname(__filename);
 
- 
+
 const app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname,'./client/build')))
+app.use(express.static(path.join(__dirname,'./client/dist')))
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoute);
