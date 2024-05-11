f7b9fcb88cf115843072cda944ec9bfcb98032b6
Pratyushgupta2003 <124146534+Pratyushgupta2003@users.noreply.github.com>
2024-05-11 14:06:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use('*' , function(req,res){
 //   });
 // });
 
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`.bgCyan.white);
